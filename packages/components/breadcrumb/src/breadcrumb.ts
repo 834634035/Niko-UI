c59689a1099c134393e70ffc49a6cbbf2f46befc
mazhihui_v <834634035@qq.com>
2025-02-04 15:31:15 +0800

    baocun
    bugfix

---
+++ packages/components/breadcrumb/src/breadcrumb.ts
@@ -0,0 +1,6 @@
+export const breadcrumbProps = {
+  separator: {
+    type: String,
+    default: "/",
+  },
+};
---
+++ packages/components/breadcrumb/src/breadcrumb.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :class="bem.b()">
+        <slot></slot>
+    </div>
+</template>
+<script lang="ts" setup>
+
+import { createNamespace } from '@niko/utils/create'
+import { breadcrumbProps } from './breadcrumb'
+
+defineOptions({
+    name: 'nk-breadcrumb'
+})
+
+const bem = createNamespace('breadcrumb')
+const props = defineProps(breadcrumbProps)
+
+
+</script>
